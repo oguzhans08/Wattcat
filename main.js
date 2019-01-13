ed0ecba26a86cc74568cfa39f20eaeecfe105d08
Oğuzhan Sağlık <45845015+oguzhans08@users.noreply.github.com>
2019-01-13 21:11:15 +0300

    (//**Değiştirilecek) JS ile giriş sağlandı (Üye ol kısmından)
    Js ile basit bir if else komutu ve biraz prompt ekleyerek giriş sağlandı.

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-$(function(){
-    $("#nav a").click(function(e){
-      e.preventDefault();
-      $('html,body').scrollTo(this.hash,this.hash); 
-    });
-  });
+function sifre()
+{
+	var kullanici, sifre;
+	kullanici=prompt("Lütfen kullanici adinizi giriniz:");
+	sifre=prompt("Lütfen şifrenizi giriniz:")
+	for(var i=1; i<=3; i++)
+	{
+	if(kullanici!="orhan" || sifre!="123")
+	{
+		alert("Yanlış kullanıcı adı ya da şifre");
+			kullanici=prompt("Kullanici adinizi tekrar giriniz");
+			sifre=prompt("Sifrenizi tekrar giriniz");									
+         }
+	else
+	{  
+             alert("Tebrikler doğru giriş");
+	     break;
+	}
+        }
+}
